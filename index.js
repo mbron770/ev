3383b806f8d31011aef23ec9138cd8911f6b5bd5
mbron770 <mbronfin.tsd@gmail.com>
2023-05-25 15:40:18 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+const dodger = document.getElementById("dodger")
+
+// dodger.style.backgroundColor = '#FF69B4'
+
+// dodger.style.bottom = "100px";
+
+// dodger.style.bottom = "0px";
+
+// dodger.style.left = "0px";
+
+document.addEventListener('keydown', function (e) {
+    if(e.key === 'ArrowLeft'){
+        moveDodgerLeft()
+    }
+})
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+
+    }
+    else {
+        moveDodgerRight()
+    }
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+
+    if (right < 300) {
+        dodger.style.left = `${right + 1}px`;
+    }
+
+}
